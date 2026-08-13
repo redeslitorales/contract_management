614c135c2f64f027df1cdeab97442f7c6599df48
redeslitorales <100039652+redeslitorales@users.noreply.github.com>
2026-08-12 21:15:32 -0600

    Updated

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
     'name': 'Cabal Contract Management',
     'author': 'Bálsamo Labs SAS',
     'maintainer': 'Bálsamo Labs SAS',
-    'version': '17.0.8.6.33',
+    'version': '17.0.8.6.35',
     'category': 'Sales Management',
     'sequence': -100,
     'summary': 'Contract Management',
